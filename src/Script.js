d6a4a18b7e52396ebea6ff80806ab4278d9411aa
GayeshmaWijerathna <109783810+GayeshmaWijerathna@users.noreply.github.com>
2023-07-29 00:28:07 +0530

    he can run now

--- src/Script.js
+++ src/Script.js
@@ -15,3 +15,51 @@ function idleAnimation(){
    idleAnimationNumber = setInterval(idleAnimation,200);
 
     }
+
+    runImageNumber = 0;
+    runAnimationNumber =0;
+
+function runAnimation(){
+
+    runImageNumber = runImageNumber+1;
+
+    if (runImageNumber==10){
+        runImageNumber=1;
+    }
+
+    boy.src = " resources/Run__00"+runImageNumber +".png";
+
+}
+
+    function runAnimationStart(){
+      runAnimationNumber =   setInterval(runAnimation,100);
+
+      clearInterval(idleAnimationNumber);
+    }
+
+    function keyCheck(event){
+        // alert(event.which);
+        // enter=13;
+
+let keyCode = event.which;
+
+if (keyCode ==13 ){
+    if (runAnimationNumber == 0){
+        runAnimationStart();
+    }
+}
+
+if (moveBackgroundAnimationID == 0){
+   moveBackgroundAnimationID =  setInterval(moveBackground,100);
+}
+    }
+
+    var backgroundImagePositionX =0;
+    var moveBackgroundAnimationID =0;
+
+    function moveBackground(){
+        backgroundImagePositionX = backgroundImagePositionX -20;
+
+    document.getElementById("background").style.backgroundPositionX = backgroundImagePositionX + "px" ;
+    }
+
