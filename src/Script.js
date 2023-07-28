950bcaf6547d0a27054ae688dd6caa4c70608053
GayeshmaWijerathna <109783810+GayeshmaWijerathna@users.noreply.github.com>
2023-07-29 01:24:24 +0530

    he can jump  now

--- src/Script.js
+++ src/Script.js
@@ -1,65 +1,118 @@
 let boy = document.getElementById("boy");
-let idleImageNumber =0;
- let idleAnimationNumber=0;
+let idleImageNumber = 1;
+let idleAnimationNumber = 0;
 
-function idleAnimation(){
+function idleAnimation() {
     idleImageNumber = idleImageNumber + 1;
 
 
-    if (idleImageNumber == 10){
+    if (idleImageNumber == 10) {
         idleImageNumber = 1;
     }
     boy.src = "resources/Idle__00" + idleImageNumber + ".png";
 }
-    function idleAnimationStart(){
-   idleAnimationNumber = setInterval(idleAnimation,200);
 
-    }
-
-    runImageNumber = 0;
-    runAnimationNumber =0;
-
-function runAnimation(){
-
-    runImageNumber = runImageNumber+1;
-
-    if (runImageNumber==10){
-        runImageNumber=1;
-    }
-
-    boy.src = " resources/Run__00"+runImageNumber +".png";
+function idleAnimationStart() {
+    idleAnimationNumber = setInterval(idleAnimation, 200);
 
 }
 
-    function runAnimationStart(){
-      runAnimationNumber =   setInterval(runAnimation,100);
+let runImageNumber = 1;
+let runAnimationNumber = 0;
 
-      clearInterval(idleAnimationNumber);
+function runAnimation() {
+
+    runImageNumber = runImageNumber + 1;
+
+    if (runImageNumber == 10) {
+        runImageNumber = 1;
     }
 
-    function keyCheck(event){
-        // alert(event.which);
-        // enter=13;
+    boy.src = " resources/Run__00" + runImageNumber + ".png";
 
-let keyCode = event.which;
+}
 
-if (keyCode ==13 ){
-    if (runAnimationNumber == 0){
+function runAnimationStart() {
+    runAnimationNumber = setInterval(runAnimation, 100);
+
+    clearInterval(idleAnimationNumber);
+}
+
+let jumpAnimationNumber = 0;
+let jumpImageNumber =0;
+let boyMarginTop = 307;
+
+function jumpAnimation(){
+    jumpImageNumber = jumpImageNumber+1;
+
+    if ( jumpImageNumber <= 5){
+        boyMarginTop = boyMarginTop - 20;
+        boy.style.marginTop = boyMarginTop + "px";
+    }
+
+    if (jumpImageNumber >= 6){
+        boyMarginTop = boyMarginTop + 20;
+        boy.style.marginTop = boyMarginTop + "px";
+    }
+
+    if (jumpImageNumber == 10){
+        jumpImageNumber =0;
+        clearInterval(jumpAnimationNumber);
+        jumpAnimationNumber =0;
+        runImageNumber = 0;
         runAnimationStart();
+
     }
+
+    boy.src = "resources/Jump__00" + jumpImageNumber + ".png";
 }
 
-if (moveBackgroundAnimationID == 0){
-   moveBackgroundAnimationID =  setInterval(moveBackground,100);
+function jumpAnimationStart(){
+    clearInterval(idleAnimationNumber);
+    runImageNumber = 0;
+    clearInterval(runAnimationNumber);
+   jumpAnimationNumber =  setInterval(jumpAnimation,100);
 }
+
+
+
+
+function keyCheck(event) {
+    // alert(event.which);
+    // enter=13;
+    //space =32;
+
+    let keyCode = event.which;
+
+    /*Enter - Run*/
+    if (keyCode == 13) {
+        if (runAnimationNumber == 0) {
+            runAnimationStart();
+        }
+        if (moveBackgroundAnimationID == 0) {
+            moveBackgroundAnimationID = setInterval(moveBackground, 100);
+        }
     }
 
-    var backgroundImagePositionX =0;
-    var moveBackgroundAnimationID =0;
+    /*Space - Jump*/
+    if (keyCode == 32) {
+        if (jumpAnimationNumber == 0){
+            jumpAnimationStart();
+        }
 
-    function moveBackground(){
-        backgroundImagePositionX = backgroundImagePositionX -20;
-
-    document.getElementById("background").style.backgroundPositionX = backgroundImagePositionX + "px" ;
+        if (moveBackgroundAnimationID == 0) {
+            moveBackgroundAnimationID = setInterval(moveBackground, 100);
+        }
     }
+
+}
+
+let backgroundImagePositionX = 0;
+let moveBackgroundAnimationID = 0;
+
+function moveBackground() {
+    backgroundImagePositionX = backgroundImagePositionX - 20;
+
+    document.getElementById("background").style.backgroundPositionX = backgroundImagePositionX + "px";
+}
 
